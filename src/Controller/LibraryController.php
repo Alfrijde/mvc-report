a5ef5070c8009aab113a4759a3189e96c031f8f8
Viktoria Arvidsson <viav23@student.bth.se>
2024-05-11 15:20:00 +0200

    First drafts

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\AbstractType;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Book;
+use App\Form\BookFormType;
+use App\Repository\BookRepository;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    #[Route('/library/add', name: 'library_add')]
+    public function createBook(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response {
+        $book = new Book();
+
+        $form = $this->createForm(BookFormType::class, $book);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+
+
+            $entityManager->persist($book);
+    
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('library_show_all');
+        }
+
+        return $this->render('library/add.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/library/show', name: 'library_show_all')]
+    public function showAllBooks(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+
+        return $this->render('library/show_all.html.twig', [
+            'books' => $books,
+        ]);
+    }
+
+    
+    #[Route('/library/show/{id}', name: 'library_show_details')]
+    public function showDetails(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        return $this->render('library/show_details.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
+    #[Route('/library/delete/{id}', name: 'delete_book')]
+    public function deleteBookById(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+    #[Route('/library/update/{id}', name: 'update_book', method:['GET'])]
+    public function updateBook(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        return $this->render('library/update.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?int $ISBN = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getISBN(): ?int
+    {
+        return $this->ISBN;
+    }
+
+    public function setISBN(int $ISBN): static
+    {
+        $this->ISBN = $ISBN;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
