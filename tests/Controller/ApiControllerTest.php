fe45bcc4568c499abab9798132e183747074e467
Viktoria Arvidsson <viav23@student.bth.se>
2024-05-16 17:58:35 +0200

    Added a test for Book

---
+++ tests/Controller/ApiControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Test cases for class ApiController.
+ */
+class ApiControllerTest extends WebTestCase
+
+{
+    /**
+ * Tests if the API home page is loading.
+ */
+    public function testApiHomePage(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        
+
+        // Request a specific page
+        $client->request('GET', '/api');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'JSON Router');
+        
+
+    }
+
+//     /**
+//  * Tests if all the books are displayed
+//   */
+
+//     public function testShowAllBooks(): void
+//     {
+//         // This calls KernelTestCase::bootKernel(), and creates a
+//         // "client" that is acting as the browser
+//         $client = static::createClient();
+        
+
+//         // Request a specific page
+//         $client->request('GET', '/library/show');
+
+//         // Validate a successful response and some content
+//         $this->assertResponseIsSuccessful();
+//         $this->assertSelectorTextContains('table', 'The Hobbit');
+//         $client->catchExceptions(false);
+//     }
+//         /**
+//  * Tests if the specified book is displayed
+//   */
+
+//   public function testShowOneBook(): void
+//   {
+//       // This calls KernelTestCase::bootKernel(), and creates a
+//       // "client" that is acting as the browser
+//       $client = static::createClient();
+      
+
+//       // Request a specific page
+//       $client->request('GET', '/library/show/1');
+
+//       // Validate a successful response and some content
+//       $this->assertResponseIsSuccessful();
+//       $this->assertSelectorTextContains('table', 'The Hobbitses');
+//       $client->catchExceptions(false);
+//   }
+}
---
+++ tests/Entity/BookTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use PHPUnit\Framework\TestCase;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectRepository;
+
+/**
+ * Test cases for the Entity Book.
+ */
+class BookTest extends TestCase
+
+{
+    public function testBook()
+    {
+        $book = new Book();
+        $book->setName('Hästen och hans pojke');
+        $book->setISBN('123456789');
+        $book->setAuthor('C.S. Lewis');
+
+        $bookRepository = $this->createMock(ObjectRepository::class);
+
+        $bookRepository->expects($this->any())
+        ->method('find')
+        ->willReturn($book);
+
+        $book = $bookRepository
+            ->find(1);
+
+        $this->assertEquals('Hästen och hans pojke', $book->getName());
+
+    }
+}
