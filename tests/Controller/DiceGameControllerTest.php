1c1cfe6718d0956161491674258f6cf5e5ac69ff
Viktoria Arvidsson <viav23@student.bth.se>
2024-05-16 14:27:10 +0200

    Second round of fixing issues

---
+++ tests/Controller/DiceGameControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test cases for class DiceGameController.
+ */
+class DiceGameCotrollerTest extends WebTestCase
+/**
+ * Tests if the init-page is loading
+ */
+{
+    public function testInitPage(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/game/pig/init');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Pig game');
+    }
+
+
+}
--- tests/Dice/DiceGraphicTest.php
+++ tests/Dice/DiceGraphicTest.php
@@ -17,10 +17,10 @@ class DiceGraphicTest extends TestCase
     {
         $die = new DiceGraphic();
         $this->assertInstanceOf("\App\Dice\DiceGraphic", $die);
-        $res1 = $die->roll();
+        $die->roll();
 
-        $res2 = $die->getAsString();
-        $this->assertNotEmpty($res2);
+        $res = $die->getAsString();
+        $this->assertNotEmpty($res);
     }
 
     /**
@@ -29,9 +29,9 @@ class DiceGraphicTest extends TestCase
     public function testGetValueDice()
     {
         $die = new DiceGraphic();
-        $res1 = $die->roll();
-        $res2 = $die->getAsString();
-        $this->assertIsString($res2);
+        $die->roll();
+        $res = $die->getAsString();
+        $this->assertIsString($res);
     }
 
 
